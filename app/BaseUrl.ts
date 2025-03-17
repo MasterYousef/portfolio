28db51d8d38d320a5325b11f893fc3476f9c462e
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2025-03-17 08:00:39 +0200

    Update BaseUrl.ts

--- app/BaseUrl.ts
+++ app/BaseUrl.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 const BaseUrl = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://worthy-antelope-o08sans80o-7acc4178.koyeb.app/",
 });
 export default BaseUrl;
