d09bbe8be710a220cd1f9b3894fbcdca906411dd
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2025-07-07 18:23:14 +0300

    Refactor Footer for improved readability and update WhatsApp link
    Reformatted the Footer component for better readability by adjusting JSX formatting and line breaks. Updated the WhatsApp link to include the country code in the phone number.

--- app/components/utility/Footer.tsx
+++ app/components/utility/Footer.tsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <footer className=" bg-zinc-900 flex  md:flex-row flex-col justify-between text-white p-5 w-full">
-      <p className="text-xl font-bold mb-5 md:mb-0">&copy; 2024 YOUSEF MOSTAFA</p>
+      <p className="text-xl font-bold mb-5 md:mb-0">
+        &copy; 2024 YOUSEF MOSTAFA
+      </p>
       <div className="icons text-white flex justify-between text-4xl w-auto ">
         <div>
           <a target="_blank" href="mailto:imayosefgo@gmail.com">
@@ -9,17 +11,23 @@ function Footer() {
           </a>
         </div>
         <div>
-          <a target="_blank" href="https://www.facebook.com/people/Yousef-Mostafa/pfbid02j2j6RMdMZ6fcLnLJFhgARLcfhCgsaeK6DW1qdiNSMNuRnT38ERxPewRgjzgXqBTvl">
+          <a
+            target="_blank"
+            href="https://www.facebook.com/people/Yousef-Mostafa/pfbid02j2j6RMdMZ6fcLnLJFhgARLcfhCgsaeK6DW1qdiNSMNuRnT38ERxPewRgjzgXqBTvl"
+          >
             <i className="fa-brands fa-facebook"></i>
           </a>
         </div>
         <div>
-          <a target="_blank" href="https://wa.me/01158122005">
+          <a target="_blank" href="https://wa.me/+201158122005">
             <i className="fa-brands fa-whatsapp"></i>
           </a>
         </div>
         <div>
-          <a target="_blank" href="https://www.linkedin.com/in/yousef-mostafa-4263b5243/">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/yousef-mostafa-4263b5243/"
+          >
             <i className="fa-brands fa-linkedin"></i>
           </a>
         </div>
